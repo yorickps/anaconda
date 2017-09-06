63bc4a890d840475679f52741d8500a09d07dbf0
Michael Speth <spethm@landcareresearch.co.nz>
2017-09-07 11:53:08 +1200

    Cleaned up formatting (puppet lint)

--- manifests/package.pp
+++ manifests/package.pp
@@ -3,30 +3,29 @@
 
 # Note: This can be very slow - it downloads packages from the Repo
 define anaconda::package( $env=undef, $base_path='/opt/anaconda') {
-    include anaconda
-    
-    $conda = "${base_path}/bin/conda"
-        
-    
-    # Need environment option if env is set
-    # Also requirement on the env being defined
-    if $env {
-        $env_option = "--name ${env}"
-        $env_require = [Class["anaconda::install"], Anaconda::Env[$env] ]
-        $env_name = "${env}"
-    } else {
-        $env_option = ''
-        $env_name = "root"
-        $env_require = [Class["anaconda::install"]]
-    }
-    
-    
-    exec { "anaconda_${env_name}_${name}":
-        command => "${conda} install --yes --quiet ${env_option} ${name}",
-        require => $env_require,
-        
-        # Ugly way to check if package is already installed
-        # bug: conda list returns 0 no matter what so we grep output
-        unless  => "${conda} list ${env_option} ${name} | grep -q -w -i '${name}'",
-    }
+  include anaconda
+
+  $conda = "${base_path}/bin/conda"
+
+  # Need environment option if env is set
+  # Also requirement on the env being defined
+  if $env {
+    $env_option  = "--name ${env}"
+    $env_require = [Class['anaconda::install'], Anaconda::Env[$env] ]
+    $env_name    = $env
+  } else {
+    $env_option  = ''
+    $env_name    = 'root'
+    $env_require = [Class['anaconda::install']]
+  }
+
+
+  exec { "anaconda_${env_name}_${name}":
+    command => "${conda} install --yes --quiet ${env_option} ${name}",
+    require => $env_require,
+
+    # Ugly way to check if package is already installed
+    # bug: conda list returns 0 no matter what so we grep output
+    unless  => "${conda} list ${env_option} ${name} | grep -q -w -i '${name}'",
+  }
 }
