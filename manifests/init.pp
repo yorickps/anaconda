073c31b9d12d5638a9a69ad1f67faf841c07caa5
Michael Speth <spethm@landcareresearch.co.nz>
2017-09-07 11:56:52 +1200

    More puppet lint fixes.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,5 @@
+# Manages the installation of anaconda.
 class anaconda {
-    Exec { path => '/opt/anaconda/bin:/usr/bin:/usr/sbin/:/bin:/sbin' }
-    include anaconda::install
+  Exec { path => '/opt/anaconda/bin:/usr/bin:/usr/sbin/:/bin:/sbin' }
+  include anaconda::install
 }
--- manifests/install.pp
+++ manifests/install.pp
@@ -1,28 +1,27 @@
+# Manages the installation of anaconda.
 class anaconda::install {
 
-    include anaconda::params
+  include anaconda::params
 
-    # Bug fix: the require will force a download even if it doesn't have to be installed
-    exec { "install_anaconda":
-        command => "bash /tmp/${anaconda::params::installer} -b -p /opt/anaconda",
-        creates => "/opt/anaconda",
-        require => Exec["download_anaconda"],
-    }
+  # Bug fix: the require will force a download even if it doesn't have to be installed
+  exec { 'install_anaconda':
+    command => "bash /tmp/${anaconda::params::installer} -b -p /opt/anaconda",
+    creates => '/opt/anaconda',
+    require => Exec['download_anaconda'],
+  }
 
-    # Bug fix: the onlyif prevents the download from happening
-    # when it doesn't need to be installed
-    # Avoid circular dependency on "install_anaconda", run if /opt/anaconda doesn't exist
-    exec { "download_anaconda":
-        command => "wget -P /tmp ${anaconda::params::url}",
-        creates => "/tmp/${anaconda::params::installer}",
-        onlyif  => "test ! -d /opt/anaconda",
-    }
+  # Bug fix: the onlyif prevents the download from happening
+  # when it doesn't need to be installed
+  # Avoid circular dependency on "install_anaconda", run if /opt/anaconda doesn't exist
+  exec { 'download_anaconda':
+    command => "wget -P /tmp ${anaconda::params::url}",
+    creates => "/tmp/${anaconda::params::installer}",
+    onlyif  => 'test ! -d /opt/anaconda',
+  }
 
-    file { "/opt/anaconda/.condarc":
-        source => "puppet:///modules/anaconda/.condarc",
-        ensure => present,
-        require => Exec["install_anaconda"],
-    }
-    
-
+  file { '/opt/anaconda/.condarc':
+    ensure  => present,
+    source  => 'puppet:///modules/anaconda/.condarc',
+    require => Exec['install_anaconda'],
+  }
 }
